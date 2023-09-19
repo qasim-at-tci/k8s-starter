0a007f22d3fc1d07f6f74f50a83a2d56cb603479
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2023-09-19 16:19:22 +0500

    health is wealth :D

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type Status struct {
+	Status string `json:"status"`
+}
+
 func main() {
 	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/_/healthz", healthCheckHandler)
 	log.Println("Starting server - Press CTRL+C to exit..")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
@@ -19,4 +25,11 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("ERROR!")
 	}
 	fmt.Fprintf(w, "Hello from %s", hostname)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	status := Status{Status: "OK"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(status)
 }
